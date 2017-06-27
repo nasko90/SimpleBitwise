22870339a4f0b91341d682551413c5ae1f3a440d
nasko90 <nasko_drenovichki@abv.bg>
2017-06-27 20:31:32 +0300

    adding some methods

---
+++ BitwiseOperations.cs
@@ -0,0 +1,69 @@
+﻿namespace ConsoleApplication1
+{
+    /// <summary>
+    /// Static class containing basic numeric methods and operations
+    /// </summary>
+    public static class BitwiseOperations
+    {
+        /// <summary>
+        /// Given two integers L and R and find the maximal value of A xor B, where A and B satisfy the following condition: L &lt;= A &lt;= B &lt;= R
+        /// </summary>
+        /// <param name="l"></param>
+        /// <param name="r"></param>
+        /// <returns>Integer - the maximum value of l xor r</returns>
+        public static int GetMaxXor(int l, int r)
+        {
+            var result = 0;
+            var xor = l ^ r;
+            var positionOfLastUnsignedBit = GetMostSignificantBitPosition(xor);
+            for (int i = 0; i <= positionOfLastUnsignedBit; i++)
+            {
+                result |= 1 << i;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Find and return the unique element in array where every element in that array occurs exactly twice except for the unique element.
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns>Return the unique element</returns>
+        public static int GetUniqueElementInArray(int[] arr)
+        {
+            var uniqueElement = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                uniqueElement ^= arr[i];
+            }
+
+            return uniqueElement;
+        }
+
+        /// <summary>
+        /// Get most siginificant bit position in 32bit integer
+        /// </summary>
+        /// <param name="number">32 bit integer</param>
+        /// <returns>The position of most significant bit</returns>
+        public static int GetMostSignificantBitPosition(int number)
+        {
+            int position = 31;
+            var firstSignBitPosition = 0;
+
+            while (position >= 0)
+            {
+                firstSignBitPosition = (number >> position) & 1;
+                if (firstSignBitPosition == 1)
+                {
+                    return position;
+                }
+                position--;
+            }
+
+            return 0;
+        }
+
+
+    }
+}
---
+++ RandomBitwiseMethods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public static class RandomBitwiseMethods
+    {
+        public static void CounterGame(int n)
+        {
+            var counter = 0;
+
+            bool isNpowerOf2 = false;
+
+            while (n > 1)
+            {
+                counter++;
+                isNpowerOf2 = (n & (n - 1)) == 0;
+
+                if (isNpowerOf2)
+                {
+                    n = n / 2;
+                }
+                else
+                {
+                    var mostSignificantBit = BitwiseOperations.GetMostSignificantBitPosition(n);
+                    var reduce = 1 << (mostSignificantBit);
+                    n = n - reduce;
+                }
+            }
+            var whoWin = counter % 2 == 0 ? "Richard" : "Louise";
+            Console.WriteLine(whoWin);
+        }
+    }
+}
